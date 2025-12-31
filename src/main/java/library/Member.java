89e8e17547d0e025d328c7ce9ed4bec9eac5e3b5
CHHAVI <143962541+CHHAVI6425@users.noreply.github.com>
2025-12-31 16:28:20 +0530

    Update Member.java

--- src/main/java/library/Member.java
+++ src/main/java/library/Member.java
@@ -1,1 +1,20 @@
 
+package library;
+
+public class Member {
+    private String id;
+    private String name;
+
+    public Member(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return id + "," + name;
+    }
+}
